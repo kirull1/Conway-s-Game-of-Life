1d8ca113da35bdd4b091ce0d220898674d3ab457
kirill22111333 <35836563+kirill22111333@users.noreply.github.com>
2021-06-04 00:57:34 +0300

    update

--- main.php
+++ main.php
@@ -3,6 +3,7 @@
     class game{
 
         private $illustration = ['░░', '▓▓'];
+        private $offset = "\n";
 
         public function __construct($map = null){
             $this->map = $map;
@@ -16,23 +17,29 @@
         }
 
         public function generate($size, $p){
-            for ($i=0; $i < $size; $i++) { 
-                for ($j=0; $j < $size; $j++) { 
+            for ($i=0; $i < $size; $i++)
+                for ($j=0; $j < $size; $j++) 
                     $matrix[$i][$j] = rand(0, 100) <= $p ? $this->illustration[1] : $this->illustration[0];
-                }
-            }
+
             $this->map = $matrix;
         }
 
-        public function compile(){
+        public function build_map($size, $elm, $indent = [0, 0]){
+            $max = count($elm);
+            for ($i=0; $i < $size; $i++)
+                for ($j=0; $j < $size; $j++) 
+                    $matrix[$i][$j] = $indent[0] <= $i && $indent[1] <= $j && $i - $indent[0] < $i && $j - $indent[1] < $j && $max + $indent[0] > $i && $max + $indent[1] > $j ? $elm[$i - $indent[0]][$j - $indent[1]] : $this->illustration[0];
+
+            $this->map = $matrix;
+        }
+
+        public function compile($indent = 0){
             $this->check_map();
-            $size = count($this->map);
+            $size = count($this->map) - $indent;
             $string = '';
-            for ($i=0; $i < $size; $i++) { 
-                for ($j=0; $j < $size; $j++) { 
-                    $string .= $this->map[$i][$j];
-                }
-                $string .= "\n";
+            for ($i=$indent; $i < $size; $i++) { 
+                for ($j=$indent; $j < $size; $j++) $string .= $this->map[$i][$j];
+                $string .= $this->offset;
             }
             return $string;
         }
@@ -50,42 +57,27 @@
 
         private function offset_map(){
             $size = count($this->map);
-            for ($i=0; $i < $size; $i++) { 
-                for ($j=0; $j < $size; $j++) { 
+            for ($i=0; $i < $size; $i++)
+                for ($j=0; $j < $size; $j++)
                     $this->map[$i][$j] = str_replace('2', $this->illustration[1], $this->map[$i][$j]);
-                }
-            }
         }
 
         public function update_map(){
+            $this->check_life(1);
             $this->check_life();
-            $this->check_die();
             $this->offset_map();
         }
 
-        private function check_die(){
+        private function check_life($type = null){
             $size = count($this->map);
             $map = $this->map;
-            for ($i=0; $i < $size; $i++) { 
-                for ($j=0; $j < $size; $j++) { 
-                    if($this->map[$i][$j] == $this->illustration[1]){
-                        $map[$i][$j] = $this->status([$i, $j]) == 1 ? $this->illustration[0] : 2;
-                    }
-                }
-            }
-            $this->map = $map;
-        }
+            for ($i=0; $i < $size; $i++)
+                for ($j=0; $j < $size; $j++)
+                    if ($type === 1) 
+                        if($this->map[$i][$j] == $this->illustration[0]) $map[$i][$j] = $this->status([$i, $j]) < 3 ? $this->illustration[0] : 2;
+                    else 
+                        if($this->map[$i][$j] == $this->illustration[1]) $map[$i][$j] = $this->status([$i, $j]) == 1 ? $this->illustration[0] : 2;
 
-        private function check_life(){
-            $size = count($this->map);
-            $map = $this->map;
-            for ($i=0; $i < $size; $i++) { 
-                for ($j=0; $j < $size; $j++) { 
-                    if($this->map[$i][$j] == $this->illustration[0]){
-                        $map[$i][$j] = $this->status([$i, $j]) < 3 ? $this->illustration[0] : 2;
-                    }
-                }
-            }
             $this->map = $map;
         }
 
